23878bda41f752b85953e8457de723f59741a6e6
Clarkinr <robclarkin1@gmail.com>
2022-03-15 18:49:38 +0000

    add main logic to tour type function

--- run.py
+++ run.py
@@ -31,16 +31,28 @@ def tour_size():
     Requests tournament size and type from the user and generates the bracket
     """
     num_teams = [4,8,16]
-    input_num_teams = int(input("Enter the number of teams taking part in the tournament (4/8/16): \n"))
+    input_num_teams = int(input("Enter the number of teams in the tournament (4/8/16): \n"))
     if input_num_teams in num_teams:
-        print(input_num_teams)
+        print(f"Your tournament will contain {input_num_teams} teams\n")
     else:
-        print("Tournament size must be 4,8 or 16")
+        print("Tournament size must be 4,8 or 16\n Please enter a valid number\n")
+        tour_size()
 
 tour_size()
 
 def tour_typ():
     print("Choose type of torunament to create\n")
     print("Your options are \n1. Single Elimination \n2. Double Elimination \n3. Round Robin \n4. Round Robin Split")
-
+    tour_select = int(input("Enter the number of the type of torunament you would like to run\n"))
+    if tour_select == 1:
+        print("You have chosen Single Elimination\n")
+    elif tour_select == 2:
+        print("You have chosen Double Elimination\n")
+    elif tour_select == 3:
+        print("You have chosen Round Robin\n")
+    elif tour_select == 4:
+        print("You have chosen Round Robin Split\n")
+    else: 
+        print("You must enter a single number 1,2,3 or 4\n please try again\n")
+        tour_typ()    
 tour_typ()
