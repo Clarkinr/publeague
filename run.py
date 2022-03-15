1d9d3acf941d91c8d3388931a6ea2562a640aa77
Clarkinr <robclarkin1@gmail.com>
2022-03-15 15:28:12 +0000

    add creds to gitignore

--- run.py
+++ run.py
@@ -2,25 +2,35 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("Welcome to the publeague tournament tracker!\n")
-tour_name = input("What is the name of your tournament\n")
-num_teams = input("Please enter the number of teams in your torunament: \n")
-print(f"Your tournament bracket will contain {num_teams} teams\n")
-print("Please select a tournament type:\n 1. Round Robin\n 2. Round Robin Double Split\n 3. Single Elimination\n 4. Double Elimination\n")
-tour_typ = input("Type number for option 1, 2, 3 or 4 : \n")
+def name_league():
+    print("Welcome to the publeague tournament tracker!\n")
+    tour_name = input("What is the name of your tournament\n")
+    return tour_name
+name_league()
+print(tour_name)
+
+def tournament_size():
+    num_teams = int(input("Please enter the number of teams in your torunament: \n"))
+    print(f"Your tournament bracket will contain {num_teams} teams\n")
+    return num_teams
+tournament_size()
+
+
+def tour_typ():
+    print("Please select a tournament type:\n 1. Round Robin\n 2. Round Robin Double Split\n 3. Single Elimination\n 4. Double Elimination\n")
+    tour_typ = input("Type number for option 1, 2, 3 or 4 : \n")
+
+def create_team_names(tournament_size):
+    team_names = []
+    for i in range(0,num_teams):
+        team = str(input())
+
+        team_names.append(team)
+    print(team_names)
+create_team_names()
+
 
 def generate_single_elim(self, num_teams, tour_typ, team_names):
     self.num_teams = num_teams
     self.tour_typ = tour_typ
     self.team_names = [team_names]
-
-list1 = list() #empty list
-
-num = input("How many elements do you want") #user tells the range(optional)
-
-#iterating till user's range is reached
-for i in range(int(num)): 
-    n = input("Enter a value ")#asking for input of 1 value 
-    list1.append(int(n))#adding that value to the list
-
-print(list1)
